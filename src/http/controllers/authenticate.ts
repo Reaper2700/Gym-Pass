01b593d581a89541866260e9847ed7ae09d0c963
Reaper2700 <rafaell_b.p@edu.unifor.br>
2025-08-12 15:05:10 -0300

    jwt aplication

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,30 +1,41 @@
-import {FastifyRequest, FastifyReply } from 'fastify'
+import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 import { InvalidCredentialsError } from '@/uses-cases/erros/invalid-credentials-error'
 import { makeAuthenticateUseCase } from '@/uses-cases/factories/make-authenticate-use-cade'
+import { sign } from 'crypto'
 
-export async function authenticate (request:FastifyRequest, reply:FastifyReply) {
+export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
     const authenticateBodySchema = z.object({
         email: z.string().email(),
         password: z.string().min(6)
     })
 
-    const {email, password} = authenticateBodySchema.parse(request.body)
+    const { email, password } = authenticateBodySchema.parse(request.body)
 
-    try{
+    try {
         const authenticateUseCase = makeAuthenticateUseCase()
 
-        await authenticateUseCase.execute({
+        const { user } = await authenticateUseCase.execute({
             email,
             password
         })
-    }catch(err){
-        if(err instanceof InvalidCredentialsError){
+
+        const token = await reply.jwtSign({}, {
+            sign: {
+                sub: user.id
+            }
+        })
+
+        return reply.status(200).send({
+            token
+        })
+    } catch (err) {
+        if (err instanceof InvalidCredentialsError) {
             return reply.status(400).send({ message: err.message })
         }
-      
+
         throw err
     }
 
-    return reply.status(200).send()
+    
 }
